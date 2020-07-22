dc5bc5cacd05445a1c87f1e8faeb3bb09a832635
Ben John <bj9@illinois.edu>
2020-07-22 18:03:21 -0500

    Created database connector and basic express server

---
+++ app/config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    HOST: 'localhost',
+    USER: 'root',
+    // password: 'ht8a,dTW*yj+kH'
+    PASSWORD: 'dbspass1234',
+    DB: 'ratemate'
+};
+
+// Slightly different than https://bezkoder.com/node-js-rest-api-express-mysql/
--- app/models/db.js
+++ app/models/db.js
@@ -1,3 +1,23 @@
+const mysql = require("mysql");
+const dbConfig = require("../config/db.config.js");
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+});
+
+// open the MySQL connection
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
+module.exports = connection;
+
+/*
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -21,3 +41,4 @@ connection.end((err) => {
   // Ensures all remaining queries are executed
   // Then sends a quit packet to the MySQL server.
 });
+*/
