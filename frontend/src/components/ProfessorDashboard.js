28923896b0832e025f0013796f0a16f02ab8f2eb
Danielle Yang <dy6@illinois.edu>
2020-07-24 21:14:49 -0500

    Table

--- frontend/src/components/ProfessorDashboard.js
+++ frontend/src/components/ProfessorDashboard.js
@@ -1,41 +1,75 @@
-import React from 'react';
-import { Table } from 'reactstrap'
+import React, { Component } from 'react';
+import './ProfessorDashboard.css'
+import { Button } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
-export class ProfessorDashboard extends React.Component {
-    render() {
-        return(
-            <Table striped>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
+export class ProfessorDashboard extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         professors: [
+            { name: 'Ben', avg_rating: 5.0 },
+            { name: 'Danielle', avg_rating: 4.7 },
+            { name: 'Andrew', avg_rating: 3.5 },
+            { name: 'Julia', avg_rating: 4.2 }
+         ]
+      }
+   }
+
+   deleteProfessor() {
+       console.log("YO");
+   }
+
+   editProfessor() {
+       console.log("edit");
+   }
+
+   addProfessor(newProfessor) {
+       var professors = this.state.professors;
+       this.setState({
+           professors: professors.append(newProfessor)
+       })
+   }
+
+    renderTableData() {
+        return this.state.professors.map((professor, index) => {
+        const { id, name, avg_rating } = professor //destructuring
+        return (
+            <tr key={id}>
+                <td> <Button id="delete" onClick={this.deleteProfessor}> Delete </Button> 
+                     <Button id="edit" onClick={this.editProfessor}> Edit </Button> 
+                    {name} </td>
+                <td>{avg_rating} </td>
+            </tr>
         )
+        })
     }
+
+    renderTableHeader() {
+        let header = Object.keys(this.state.professors[0])
+        return header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+     }
+  
+     render() {
+        return (
+           <div>
+              <h1 id='title'>Professors</h1>
+              <table id='professors'>
+                 <tbody>
+                    <tr>{this.renderTableHeader()}</tr>
+                    {this.renderTableData()}
+                    <tr>
+                        <td> <Input placeholder="Add professor" /> </td>
+                        <td> <Button onClick={this.addProfessor}> Add Professor </Button> </td>
+                    </tr>
+                 </tbody>
+              </table>
+           </div>
+        )
+     }
 }
 
-export default ProfessorDashboard;
+export default ProfessorDashboard; //exporting a component make it reusable and this is the beauty of react
