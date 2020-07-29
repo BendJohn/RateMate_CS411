551421f17ae67e92926dbd0148c0c891c225b5b7
Danielle Yang <dy6@illinois.edu>
2020-07-28 23:57:44 -0500

    Added Table code to every page

--- frontend/src/components/Enrollments.js
+++ frontend/src/components/Enrollments.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { NavBar } from './NavBar'
+import { NavBar } from './NavBar';
+import { Button } from 'reactstrap';
 
 export class Enrollments extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            enrollments: []
+            // List of courses
+            enrollments: [
+                {
+                    professor_name: "John Smith", 
+                    CRN: 123, 
+                    name: "Intro to CS",
+                    comments: "Comments!", 
+                    availability: true,
+                    avgGpa: 3.33,
+                    requirements_filled: "Science Gen Ed",
+                    prerequisites: "123"
+                },
+            ]
         };
     }
 
@@ -17,12 +30,44 @@ export class Enrollments extends React.Component {
         }
     }
 
+    renderTableData() {
+        if (this.state.enrollments.length === 0) {
+            return;
+        }
+
+        return this.state.enrollments.map((course, index) => {
+        const { professor_name, CRN, name, comments, availability, avgGpa, requirements_filled, prerequisites } = course //destructuring
+        return (
+            <tr key={CRN}>
+                <td> 
+                    <Button id="delete"> Enroll </Button> &nbsp;
+                    {CRN}
+                </td>
+                <td>{name} </td>
+                <td>{professor_name} </td>
+                <td>{avgGpa} </td>
+                <td> {availability ? ("Open") : ("Closed")} </td>
+                <td>{requirements_filled} </td>
+                <td>{prerequisites} </td>
+                <td>{comments} </td>
+            </tr>
+        )})
+    }
+
     render() {
         return (
             <>
             <NavBar/>
             <h1 id='title'> Enrollments Page </h1>
             {console.log(this.state.enrollments)}
+
+            <table id='table'>
+                    <tbody>
+                        <tr> <th> CRN </th> <th> Name </th> <th> Professor </th> <th> Avg GPA </th> <th> Availability </th>
+                        <th> Requirements Filled </th> <th> Prerequisites </th> <th> Comments </th> </tr>
+                        {this.renderTableData()}
+                    </tbody>
+            </table>
             </>
         )
     }
--- frontend/src/components/Recommendations.js
+++ frontend/src/components/Recommendations.js
@@ -6,9 +6,55 @@ export class Recommendations extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            recs: [],
+            // List of courses
+            recs: [
+                {
+                    professor_name: "Ben John", 
+                    CRN: 312, 
+                    name: "Disapproving Nicknames 101",
+                    comments: "Comments!", 
+                    availability: true,
+                    avgGpa: 3.23,
+                    requirements_filled: "",
+                    prerequisites: "456"
+                },
+                {
+                    professor_name: "Pocahontas", 
+                    CRN: 333, 
+                    name: "Advanced Algebra",
+                    comments: "Comments!", 
+                    availability: false,
+                    avgGpa: 4.0,
+                    requirements_filled: "English Gen Ed",
+                    prerequisites: "222"
+                }
+            ],
             enrollments: ["props passed from recommendations"]
         };
+    }
+
+    renderTableData() {
+        if (this.state.recs.length === 0) {
+            return;
+        }
+
+        return this.state.recs.map((course, index) => {
+        const { professor_name, CRN, name, comments, availability, avgGpa, requirements_filled, prerequisites } = course //destructuring
+        return (
+            <tr key={CRN}>
+                <td> 
+                    <Button id="delete"> Enroll </Button> &nbsp;
+                    {CRN}
+                </td>
+                <td>{name} </td>
+                <td>{professor_name} </td>
+                <td>{avgGpa} </td>
+                <td> {availability ? ("Open") : ("Closed")} </td>
+                <td>{requirements_filled} </td>
+                <td>{prerequisites} </td>
+                <td>{comments} </td>
+            </tr>
+        )})
     }
 
 
@@ -19,7 +65,15 @@ export class Recommendations extends React.Component {
                     enrollments={this.state.enrollments}
                 />
 
-            <h1 id='title'> Recommendations </h1>
+                <h1 id='title'> Recommendations </h1>
+
+                <table id='table'>
+                    <tbody>
+                        <tr> <th> CRN </th> <th> Name </th> <th> Professor </th> <th> Avg GPA </th> <th> Availability </th>
+                        <th> Requirements Filled </th> <th> Prerequisites </th> <th> Comments </th> </tr>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
             </>
         )
     }
