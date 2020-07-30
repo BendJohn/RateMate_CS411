937e4e31ffe207b7d1015d0ce7b65d18523cb771
BendJohn <42980091+BendJohn@users.noreply.github.com>
2020-07-29 20:51:07 -0500

    added route for basicSearch

--- app/routes/basicSearch.routes.js
+++ app/routes/basicSearch.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
     const basicsearch = require("../controllers/basicSearch.controller.js");
   
     // Retrieve all basicsearch
-    app.get("/basicsearch/:crn/:rtg_upper/:rtg_lower/:gpa_upper/:gpa_lower", basicsearch.findWithCrn);
+    app.get("/basicsearch/:crn", basicsearch.findWithCrn);
   
     // Retrieve a single basicsearch with basicsearch
     app.get("/basicsearch", basicsearch.findWithoutCrn);
