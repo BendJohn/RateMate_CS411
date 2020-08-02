dadf049799016f27af0fac432ba27de75d400bfd
BendJohn <42980091+BendJohn@users.noreply.github.com>
2020-08-02 13:58:20 -0500

    Added new recommendation stuff

--- app/recommendation/recommender.py
+++ app/recommendation/recommender.py
@@ -11,14 +11,43 @@ from scipy import sparse
 # Prerequisite Graph
 #--------------------
 
-def prerequisite():
-    pass
+def prerequisite(netid, standing, department, user_courses):
+    data = pd.read_csv('./app/data/prerequisites.csv')
+    data_dept = data[data.Course.str.startswith(department + ' ')]
+
+    # Add all CS prerequisite courses
+    for uCourse in user_courses:
+        uCourse_prereqs = data_dept.loc[data_dept['Course'] == uCourse]
+        num_prereqs = uCourse_prereqs.iloc[0]['PrerequisiteNumber']
+        for i in range(num_prereqs):
+            prereq = uCourse_prereqs.iloc[0][str(i)]
+            if prereq.startswith(department + ' ') and prereq not in user_courses:
+                user_courses.append(prereq)
+
+    # Go through all department courses and check whether user can take them
+    recCourses = []
+    for index, row in data_dept.iterrows():
+        cur_course = row['Course']
+        if (cur_course in user_courses):
+            continue
+
+        num_prereqs = row['PrerequisiteNumber']
+        add = True
+        for i in range(num_prereqs):
+            prereq = row[str(i)]
+            if prereq.startswith(department + ' ') and prereq not in user_courses:
+                add = False
+                break
+        if add:
+            recCourses.append(cur_course)
+
+    # 
 
 #----------------------------------
 # Nearest Neighbors Recommendation
 #----------------------------------
 
-def nearestNeighbors():
+def nearestNeighbors(netid, standing, department, user_courses):
     data = pd.read_csv('./app/data/courses.csv')
 
     # Create a new dataframe without the user ids.
@@ -90,7 +119,7 @@ def nearestNeighbors():
 
     # Get the artists the user has played.
     known_user_likes = data_items.iloc[user_index]
-    known_user_likes = known_user_likes[known_user_likes >0].index.values
+    known_user_likes = known_user_likes[known_user_likes > 0].index.values
 
     # Construct the neighborhood from the most similar items to the
     # ones our user has already liked.
@@ -118,6 +147,8 @@ def nearestNeighbors():
 # Choose Recommender
 #--------------------
 
+netid = sys.argv[1]
+
 # Run SQL query to find out how many unique users there are\import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
@@ -126,10 +157,23 @@ mydb = mysql.connector.connect(
   database="ratemate"
 )
 
+# How many users are there?
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM user")
-myresult = mycursor.fetchall()
-if len(myresult) > 100:
-    prerequisite()
+mycursor.execute("SELECT count(*) FROM user")
+users = mycursor.fetchall()
+
+# Get the department and standing of the user
+mycursor.execute("SELECT standing, department FROM user WHERE netid='" + netid + "'")
+tmp = mycursor.fetchall()
+standing = tmp[0][0]
+department = tmp[0][1]
+
+# Get all courses the user has taken
+mycursor.execute("SELECT CONCAT(s.subject, ' ', s.number) as course FROM enrollments e NATURAL JOIN section s WHERE netid='" + netid + "'")
+courses = mycursor.fetchall()
+courses = [elem[0] for elem in courses]
+
+if users[0][0] < 100:
+    prerequisite(netid, standing, department, courses)
 else:
-    nearestNeighbors()
+    nearestNeighbors(netid, standing, department, courses)
